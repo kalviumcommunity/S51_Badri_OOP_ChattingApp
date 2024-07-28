dffea8d95657071d9f7989be57cfabbf69a2be36
heybadrinath <heybadrinath@gmail.com>
2024-07-28 15:05:47 +0530

    used this keyword and also added methods to chatapp and chathistory

--- Main.cpp
+++ Main.cpp
@@ -22,18 +22,25 @@ public:
 // Constructor for User
 User::User(string uname, string pwd) : username(uname), password(pwd), isLoggedIn(false) {}
 
+bool User::login(string pwd)
+{
+    if (this->password == pwd)
+    {
+        this->isLoggedIn = true;
+        return true;
+    }
+    return false;
+}
+
 void User::logout()
 {
-    isLoggedIn = false;
+    this->isLoggedIn = false;
 }
 
 string User::getUsername() const
 {
-    return username;
+    return this->username;
 }
-
-
-
 
 // Derived NormalUser Class
 class NormalUser : public User
@@ -44,8 +51,6 @@ public:
 
 // Constructor for NormalUser
 NormalUser::NormalUser(string uname, string pwd) : User(uname, pwd) {}
-
-
 
 // Derived Admin Class
 class Admin : public User
@@ -59,10 +64,6 @@ public:
 
 // Constructor for Admin
 Admin::Admin(string uname, string pwd, string AdminKey) : User(uname, pwd), adminKey(AdminKey) {}
-
-
-
-
 
 // Message Class
 class Message
@@ -84,17 +85,17 @@ Message::Message(string sndr, string rcvr, string cntnt) : sender(sndr), receive
 
 string Message::getSender() const
 {
-    return sender;
+    return this->sender;
 }
 
 string Message::getReceiver() const
 {
-    return receiver;
+    return this->receiver;
 }
 
 string Message::getContent() const
 {
-    return content;
+    return this->content;
 }
 
 // ChatHistory Class
@@ -106,7 +107,7 @@ private:
 public:
     ChatHistory();
     void addMessage(const Message &message);
-    // void showMessages(const string &user1, const string &user2) const;
+    vector<Message> getMessages() const;
 };
 
 // Constructor for ChatHistory
@@ -114,7 +115,12 @@ ChatHistory::ChatHistory() {}
 
 void ChatHistory::addMessage(const Message &message)
 {
-    messages.push_back(message);
+    this->messages.push_back(message);
+}
+
+vector<Message> ChatHistory::getMessages() const
+{
+    return this->messages;
 }
 
 //-----------------------------------------------------------------------------
@@ -129,48 +135,119 @@ public:
     ChatHistory chatHistory;
     ChatApp();
     void registerUser(string uname, string pwd, bool isAdmin, string adminKey = "");
-    // bool loginUser(string uname, string pwd);
+    bool loginUser(string uname, string pwd);
     void logoutUser();
-    // User *findUserByUsername(const string &uname);
-    // void sendMessage(const string &receiver, const string &content);
-    // void showChatHistory(const string &user1, const string &user2) const; // Public method to access chatHistory
+    User *findUserByUsername(const string &uname);
+    void sendMessage(const string &receiver, const string &content);
+    void showChatHistory(const string &user1, const string &user2) const;
 };
 
 // Constructor for ChatApp
 ChatApp::ChatApp() : currentUser(nullptr) {}
 
-
 void ChatApp::registerUser(string uname, string pwd, bool isAdmin, string adminKey)
 {
-    if (adminKey == "heybadrinath")
+    if (isAdmin && adminKey == "heybadrinath")
     {
-        users.push_back(new Admin(uname, pwd, adminKey));
+        this->users.push_back(new Admin(uname, pwd, adminKey));
     }
     else
     {
-        users.push_back(new NormalUser(uname, pwd));
-        cout << "register user" << uname << pwd << adminKey;
+        this->users.push_back(new NormalUser(uname, pwd));
     }
 }
 
+bool ChatApp::loginUser(string uname, string pwd)
+{
+    User *user = this->findUserByUsername(uname);
+    if (user && user->login(pwd))
+    {
+        this->currentUser = user;
+        return true;
+    }
+    return false;
+}
 
 void ChatApp::logoutUser()
 {
-    if (currentUser)
+    if (this->currentUser)
     {
-        currentUser->logout();
-        currentUser = nullptr;
+        this->currentUser->logout();
+        this->currentUser = nullptr;
+        cout << "user logged out" << endl;
     }
 }
 
+User *ChatApp::findUserByUsername(const string &uname)
+{
+    for (auto user : this->users)
+    {
+        if (user->getUsername() == uname)
+        {
+            return user;
+        }
+    }
+    return nullptr;
+}
 
+void ChatApp::sendMessage(const string &receiver, const string &content)
+{
+    if (this->currentUser)
+    {
+        Message msg(this->currentUser->getUsername(), receiver, content);
+        this->chatHistory.addMessage(msg);
+    }
+    else
+    {
+        cout << "No user is currently logged in!" << endl;
+    }
+}
 
+void ChatApp::showChatHistory(const string &user1, const string &user2) const
+{
+    for (const auto &message : this->chatHistory.getMessages())
+    {
+        if ((message.getSender() == user1 && message.getReceiver() == user2) || (message.getReceiver() == user1 && message.getSender() == user2))
+        {
+            cout << message.getSender() << " to " << message.getReceiver() << ": " << message.getContent() << endl;
+        }
+    }
+}
 
 int main()
 {
     ChatApp app;
-    
-    app.registerUser("badri", "badriPass",true,"heybadrinath");
+
+    app.registerUser("badri", "badriPass", true, "heybadrinath");
+    app.registerUser("alice", "alicePass", false);
+
+    if (app.loginUser("badri", "badriPass"))
+    {
+        cout << "User 'badri' logged in successfully." << endl;
+    }
+    else
+    {
+        cout << "Failed to log in user 'badri'." << endl;
+    }
+
+    app.sendMessage("alice", "Hello, Alice!");
+
+    app.showChatHistory("badri", "alice");
+
+    app.logoutUser();
+
+    if (app.loginUser("alice", "alicePass"))
+    {
+        cout << "User 'alice' logged in successfully." << endl;
+    }
+    else
+    {
+        cout << "Failed to log in user 'alice'." << endl;
+    }
+
+    app.showChatHistory("alice", "badri");
+
+    app.logoutUser();
 
     return 0;
 }
